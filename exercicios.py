b7ab922cbf76be20c47264450674e0f69fd3e3d0
Matheusinhofs <matheusfs117@gmail.com>
2024-09-10 23:00:44 -0300

    Exercicio 4

--- exercicios.py
+++ exercicios.py
@@ -17,18 +17,27 @@ import math
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-numero_1 = float(input("Digite o primeiro número: "))
+# numero_1 = float(input("Digite o primeiro número: "))
 
-if isinstance(numero_1,float):
-    resto_divisao = numero_1 % 5 
-    print(f"O resto da divisão dos números é {resto_divisao}")
-else:
-    print("Tipo de dado não compatível, por favor digite um número")  # Imprime a mensagem de erro:
-    # print("Divisão por zero não aceita")
+# if isinstance(numero_1,float):
+#     resto_divisao = numero_1 % 5 
+#     print(f"O resto da divisão dos números é {resto_divisao}")
+# else:
+#     print("Tipo de dado não compatível, por favor digite um número")  # Imprime a mensagem de erro:
+#     # print("Divisão por zero não aceita")
     
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
+# try:
+#     numero_1 = float(input("Digite um número: "))
+#     numero_2 = float(input("Digite um segundo número: "))
+#     multiplicacao = numero_1 * numero_2
+# except ValueError:
+#     print("Digite um número válido")
+# else: 
+#     print(f"A mutiplicação de {numero_1} por {numero_2} é igual a {multiplicacao}")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
 # numero_1 = int(input("Digite o primeiro número: "))
 # numero_2 = int(input("Digite o segundo número: "))
@@ -37,8 +46,9 @@ else:
 
 # print(div_inteira)
 
-
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+
 
 # #### Números de Ponto Flutuante (`float`)
 
